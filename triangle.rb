0553c792197bc64a31eb399f8af9e1abf6de0789
David Abramowitz <david.xesllc@gmail.com>
2015-03-18 08:09:58 -0600

    161/282

--- triangle.rb
+++ triangle.rb
@@ -14,8 +14,23 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  array = [a, b, c]
+  if (a*b*c)*
+     (a + a.abs)*(b + b.abs)*(c + c.abs)*
+     (array.sort![0] + array.sort![1] - array.sort![2] + ((array.sort![0] + array.sort![1] - array.sort![2]).abs)) != 0
+    if a == b && b ==c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  else
+    raise TriangleError.new(:TriangleError)
+  end
 end
+
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
